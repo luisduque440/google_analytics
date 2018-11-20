df576a2b4c8464c0a7f9475aa4d16240ecf7795e
Luis Duque <lduque@indeed.com>
2018-11-20 14:38:36 -0600

    modifications done overnight

--- data_wrangling.py
+++ data_wrangling.py
@@ -9,7 +9,30 @@ from pandas.io.json import json_normalize
 from sklearn import preprocessing
 
 
-def get_training_data(df , first_train_day):
+def get_training_data_from_visits(df):
+    """ 
+
+    Args:
+        df: 
+        first_train_day:
+        
+    Returns:
+        DataFrame: one row for each fullVisitorId in df
+    """
+
+    first_train_day = df.date.min()
+    last_train_day = df.date.max()
+
+    all_training_periods = []
+    while(first_train_day + timedelta(days=275) <= last_train_day):  ## 276 days is around 9months
+        all_training_periods.append(get_training_data_in_a_period(df , first_train_day = first_train_day))
+        first_train_day = first_train_day + timedelta(days=28) ## 4weeks = 1 month
+    return pd.concat(all_training_periods, ignore_index=True)
+
+
+    
+
+def get_training_data_in_a_period(df , first_train_day):
     """ 
 
     Args:
@@ -23,11 +46,11 @@ def get_training_data(df , first_train_day):
     first_test_day = last_train_day + timedelta(days=46)
     last_test_day = first_test_day + timedelta(days=62)
 
-    train_visits = df[(df.date>= first_train_day) & (df.date<last_train_day)]
-    test_visits = df[(df.date>=first_test_day) & (df.date<last_test_day)]
+    train_period = df[(df.date>= first_train_day) & (df.date<last_train_day)]
+    test_period = df[(df.date>=first_test_day) & (df.date<last_test_day)]
 
-    y = get_target(train_visits, test_visits)
-    X = create_features(train_visits)
+    y = get_target(train_period, test_period)
+    X = create_features(train_period)
     X['target']=y.target
     return X
 
@@ -40,22 +63,20 @@ def create_features(df):
     Returns:
         DataFrame: one row for each fullVisitorId in df
     """
-    last_train_day = df.date.max()
+    last_day = df.date.max()
     feature_dfs=[]
 
     feature_dfs.append(get_fixed_fields(df)) 
 
-    for i in [1,2,3,6]:
-        start = last_train_day - timedelta(days=i*30)
-        end = last_train_day
-        suffix = "_last_%s_months" %(i)
-        feature_dfs.append(get_cummulate_numeric_fields(df, start, end, suffix=suffix))
+    for i in [1,3,6]:
+        start = last_day - timedelta(days=i*28)
+        end = last_day
+        feature_dfs.append(get_cummulate_numeric_fields(df, start, end, suffix="_last_%s_months" %(i)))
 
     for i in [2,3,4]:
-        start = last_train_day - timedelta(days=i*30)
-        end = start + timedelta(days=30)
-        suffix = "_%s_months_ago" %(i)
-        feature_dfs.append(get_cummulate_numeric_fields(df, start, end, suffix=suffix))
+        start = last_day - timedelta(days=i*28)
+        end = start + timedelta(days=28)
+        feature_dfs.append(get_cummulate_numeric_fields(df, start, end, suffix="_%s_months_ago" %(i)))
     
     return pd.concat(feature_dfs, axis=1)
 
@@ -72,16 +93,13 @@ def get_cummulate_numeric_fields(df, start_date, end_date, suffix=''):
             one column per float64 field
     """
     numeric_column_names = df.select_dtypes(include='float64').columns
-    
     result = pd.DataFrame(index=df.fullVisitorId.unique())
     result.index.name= 'fullVisitorId'
-
     relevant_df = df[(df.date>= start_date)& (df.date<=end_date)]
-    different_visitors = relevant_df.groupby('fullVisitorId')
     
+    grouped_df = relevant_df.groupby('fullVisitorId')
     for col in numeric_column_names:
-        cummulative_sum = different_visitors[col].sum()
-        result[col+suffix] = cummulative_sum
+        result[col+suffix] = grouped_df[col].sum()
     return result.fillna(0)
 
 
@@ -99,42 +117,37 @@ def get_fixed_fields(df, suffix=''):
     
     object_column_names = list(df.select_dtypes(include='object').columns) 
     object_column_names.remove('fullVisitorId')
-    
     result = pd.DataFrame(index=df.fullVisitorId.unique())
     result.index.name= 'fullVisitorId'
-    different_visitors = df.groupby('fullVisitorId')
     
+    grouped_df = df.groupby('fullVisitorId')
     for col in object_column_names:
-        most_common = different_visitors[col].last()
-        result[col+suffix] = most_common
+        result[col+suffix] = grouped_df[col].last()
     return result
 
 
-def get_target(train_visits, test_visits):
+def get_target(train_period, test_period):
     """gets the target.
  
     Args:
-        train_visits: DataFrame. Each row is a visit in the past
-        test_visits: DataFrame. Each row is a visit in the future
+        train_period: DataFrame. Each row is a visit in the past
+        test_period: DataFrame. Each row is a visit in the future
 
  
     Returns:
         DataFrame: one row for each fullVisitorId in train_visit and
             one column (log_total_spent) with the natural log 
-            of the sum of all transactions in test_visits dataframe
+            of the sum of all transactions in test_period dataframe
     """
-    target = pd.DataFrame(index=train_visits.fullVisitorId.unique())
+    target = pd.DataFrame(index=train_period.fullVisitorId.unique())
     target.index.name = 'fullVisitorId'
-    total_spent = test_visits.groupby('fullVisitorId')['totals_totalTransactionRevenue'].agg(np.sum)
-    target['total_spent']=total_spent
+    target['total_spent'] = test_period.groupby('fullVisitorId')['totals_totalTransactionRevenue'].agg(np.sum)
     target = target.fillna(0)
     target['target']= target.total_spent.apply(lambda x: np.log(x+1))
     target=target.drop(columns=['total_spent'])
     return target
 
-
-
-def label_encode_object_dtypes(df):
+def label_encode_object_dtypes(encoder_trainer, df_to_encode):
     """Label encodes all the columns of a DataFrames df1 and df2 that have
        dtype='object'
  
@@ -144,13 +157,16 @@ def label_encode_object_dtypes(df):
     Returns:
         DataFrame: df with object types encoded.
     """
-    object_column_names = list(df.select_dtypes(include='object').columns)    
-    
+    object_column_names = df_to_encode.select_dtypes(include='object').columns  
+    encoded = df_to_encode.copy()
     for col in object_column_names:
-        lbl = preprocessing.LabelEncoder()
-        lbl.fit(df[col].values)
-        df[col] = lbl.transform(df[col].values)
-    return df
+        importance = encoder_trainer.groupby(col)['target'].agg([np.sum, np.size])
+        bayes_dictionary = importance['sum']/(importance['size']+1)
+        encoded[col] = encoded[col].apply(lambda x: bayes_dictionary[x] if x in bayes_dictionary.index else 0)
+    return encoded
+
+
+
 
 
 
@@ -162,10 +178,7 @@ def fill_empty_values(df):
     Returns:
         DataFrame: df1 with filled empty values and columns types formated
     """
-    df['totals_newVisits'] = df['totals_newVisits'].astype('object')
-    df['totals_bounces'] = df['totals_bounces'].astype('object')
-    df['device_isMobile'] = df['device_isMobile'].astype('object')
-
+    
     object_column_names = df.select_dtypes(include='object').columns 
     for col in object_column_names:
         df[col] = df[col].apply(unicode).apply(lambda x:x.encode('utf-8'))
@@ -177,6 +190,8 @@ def fill_empty_values(df):
     df['date'] = pd.to_datetime(df.visitStartTime, unit='s').astype('datetime64')
     df = df.drop(columns = ['visitStartTime', 'visitId'])
     df = df.fillna(0)
+    df['visits'] = 1.0 
+    df['paying_visits'] = (df.totals_totalTransactionRevenue>0).apply(float)
     return df
 
 
